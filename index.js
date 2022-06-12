7f08a7d49967e1adc2a53ae3dc4075c81b32c2a6
rakibphero <rakibrowshoncse25@gmail.com>
2022-06-13 04:44:31 +0600

    booked done in server side

--- index.js
+++ index.js
@@ -30,7 +30,29 @@ async function run(){
         });
 
 
+        app.get('/available', async(req, res)=>{
+          const date = req.query.date || 'Jun 8, 2022';
 
+          // step 1: get all services
+
+          const services = await serviceCollection.find().toArray();
+
+          // step 2: get the booking of the day
+
+          const query = {date: date};
+          const bookings = await bookingCollection.find(query).toArray();
+
+          // step 3: for each service,find bookings for that service
+
+          services.forEach(service =>{
+            const serviceBookings = bookings.filter(b => b.treatment === service.name);
+            const booked = serviceBookings.map(s => s.slot);
+            const available = service.slots.filter(s => !booked.includes(s));
+            service.available = available;
+          })
+
+          res.send(services);
+        })
 
         /**
          * API Naming Convention
