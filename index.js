ccda597ebc3b562c1b6e2981f0f0b68bc009e7c8
rakibphero <rakibrowshoncse25@gmail.com>
2022-06-14 03:45:54 +0600

    users section added on mongodb and put user for email

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run(){
 
         const serviceCollection =client.db('doctors_portal').collection('services');
         const bookingCollection =client.db('doctors_portal').collection('bookings');
+        const userCollection =client.db('doctors_portal').collection('users');
 
         app.get('/service', async(req, res) => {
             const query = {};
@@ -28,6 +29,18 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        app.get('/user/:email', async(req, res) =>{
+          const email = req.params.email;
+          const user = req.body;
+          const filter = {email: email};
+          const options = { upsert: true }; 
+          const updateDoc = {
+            $set: user,
+          };
+          const result = await userCollection.updateOne(filter, updateDoc, options);
+          res.send(result);
+        })
 
         // warning: This is not the proper way to query multiple collection,
         // after learning more about mongodb.user aggregate, lookup,pipeline,match,group
