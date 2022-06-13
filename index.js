a3d238759bc0ec47faffd4f3040d44b751c6b0df
rakibphero <rakibrowshoncse25@gmail.com>
2022-06-14 04:41:44 +0600

    token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -30,16 +31,17 @@ async function run(){
             res.send(services);
         });
 
-        app.get('/user/:email', async(req, res) =>{
+        app.put('/user/:email', async (req, res) => {
           const email = req.params.email;
           const user = req.body;
-          const filter = {email: email};
-          const options = { upsert: true }; 
+          const filter = { email: email };
+          const options = { upsert: true };
           const updateDoc = {
             $set: user,
           };
           const result = await userCollection.updateOne(filter, updateDoc, options);
-          res.send(result);
+          const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+          res.send({result, token});
         })
 
         // warning: This is not the proper way to query multiple collection,
