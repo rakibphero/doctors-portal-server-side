d3e374cd62fa60b01c0680edfddee1eacf869a49
rakibphero <rakibrowshoncse25@gmail.com>
2022-06-17 03:12:01 +0600

    admin api added

--- index.js
+++ index.js
@@ -47,10 +47,20 @@ async function run(){
             res.send(services);
         });
 
-        app.get('/user', async(req, res) =>{
+        app.get('/user',verifyJWT, async(req, res) =>{
           const users = await userCollection.find().toArray();
           res.send(users);
-        })
+        });
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+          const email = req.params.email;
+          const filter = { email: email };
+          const updateDoc = {
+            $set: {role:'admin'},
+          };
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result);
+        });
 
         app.put('/user/:email', async (req, res) => {
           const email = req.params.email;
